db823596f3d3844504393f75a2c760eb9900f77f
Ivan Mendoza <ivan.mendoza@externos-cl.cencosud.com>
2022-01-19 11:53:17 -0400

    Agregando servicio y controlador

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TripModule } from './trip/trip.module';
 
 @Module({
-  imports: [],
+  imports: [TripModule],
   controllers: [AppController],
   providers: [AppService],
 })
